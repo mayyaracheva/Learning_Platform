145a14668e8c442b355aac3e000635f1ed11ef6a
ZackYo <zarko.y@gmail.com>
2022-05-29 23:34:06 +0300

    #10 SetUp Course Repository & Service Classes/Interfaces

--- Poodle/Poodle.Services/Contracts/ICoursesService.cs
+++ Poodle/Poodle.Services/Contracts/ICoursesService.cs
@@ -5,16 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Poodle.Repositories.Contracts
+namespace Poodle.Services.Contracts
 {
-	public interface ICoursesRepository
+	public interface ICoursesService
 	{
 		Task<IEnumerable<Course>> GetAllAsync();
 		Course GetById(int id);
-		Course GetByTitle(string title);
 		IQueryable<Course> Get(CourseQueryParameters filterParameters);
-		Task<Course> CreateAsync(Course course);
-		Task<Course> UpdateAsync(int id, Course course);
+		Task<Course> Create(Course course);
+		Task<Course> Update(int id, Course course);
 		Task<Course> DeleteAsync(int id);
+
 	}
 }
---
+++ Poodle/Poodle.Services/CoursesService.cs
@@ -0,0 +1,47 @@
+﻿using Poodle.Data.EntityModels;
+using Poodle.Repositories.Contracts;
+using Poodle.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poodle.Services
+{
+	public class CoursesService : ICoursesService
+	{
+		private readonly ICoursesRepository repository;
+
+		public CoursesService(ICoursesRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		public Task<IEnumerable<Course>> GetAllAsync()
+		{
+			return this.repository.GetAllAsync();
+		}
+		public Course GetById(int id)
+		{
+			return this.repository.GetById(id);
+		}
+		public IQueryable<Course> Get(CourseQueryParameters filterParameters)
+		{
+			throw new NotImplementedException();
+		}
+		public Task<Course> Create(Course course)
+		{
+			throw new NotImplementedException();
+		}
+		public Task<Course> Update(int id, Course course)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Course> DeleteAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
